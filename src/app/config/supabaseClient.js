3e8f895cfd43c0347bed6917adc3f7d9bab856d9
ahmednule <nuleahmed6@gmail.com>
2024-09-15 00:36:35 +0300

    updated the signing up opton to use suoabase auth instead ofusing alot of code and theme supa is around

--- src/app/config/supabaseClient.js
+++ src/app/config/supabaseClient.js
@@ -1,6 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
